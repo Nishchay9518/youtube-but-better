11fba3222b3783aefd33f6ec08bb2f0fc7a3c11f
NishchayWankar <71606731+Nishchay9518@users.noreply.github.com>
2022-01-07 12:24:12 +0530

    demo added

--- src/components/videoMetaData/VideoMetaData.js
+++ src/components/videoMetaData/VideoMetaData.js
@@ -1,120 +1,98 @@
-import React, { useEffect } from 'react'
-import './videoMetaData.scss'
-import moment from 'moment'
-import numeral from 'numeral'
-
-import ShowMoreText from 'react-show-more-text'
-import { FaEye, FaHeart, FaHeartBroken } from 'react-icons/fa'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import "./videoMetaData.scss";
+import moment from "moment";
+import numeral from "numeral";
+import ShowMoreText from "react-show-more-text";
+import { FaEye, FaHeart, FaHeartBroken } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
 import {
-   checkSubscriptionStatus,
-   getChannelDetails,
-} from '../../redux/actions/channel.action'
+  checkSubscriptionStatus,
+  getChannelDetails,
+} from "../../redux/actions/channel.action";
 // import HelmetCustom from '../HelmetCustom'
 const VideoMetaData = ({ video: { snippet, statistics }, videoId }) => {
-   const { channelId, channelTitle, description, title, publishedAt } = snippet
-   const { viewCount, likeCount, dislikeCount } = statistics
+  const { channelId, channelTitle, description, title, publishedAt } = snippet;
+  const { viewCount, likeCount, dislikeCount } = statistics;
 
-   const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-   const {
-      snippet: channelSnippet,
-      statistics: channelStatistics,
-   } = useSelector(state => state.channelDetails.channel)
+  const { snippet: channelSnippet, statistics: channelStatistics } =
+    useSelector((state) => state.channelDetails.channel);
 
-   const subscriptionStatus = useSelector(
-      state => state.channelDetails.subscriptionStatus
-   )
+  const subscriptionStatus = useSelector(
+    (state) => state.channelDetails.subscriptionStatus
+  );
 
-   useEffect(() => {
-      dispatch(getChannelDetails(channelId))
-      dispatch(checkSubscriptionStatus(channelId))
-   }, [dispatch, channelId])
+  useEffect(() => {
+    dispatch(getChannelDetails(channelId));
+    dispatch(checkSubscriptionStatus(channelId));
+  }, [dispatch, channelId]);
 
-   return (
-      <div className='mx-5  py-1 videoMetaData'>
-         {/* <HelmetCustom title={title} description={description} /> */}
+  return (
+    <div className="mx-5  py-1 videoMetaData">
+      {/* <HelmetCustom title={title} description={description} /> */}
 
-         <div className=' videoMetaData__top'>
-         <div className='py-1 d-flex justify-content-between align-items-center'>
-               <span className=' days-ago'>
-                  {moment( publishedAt).fromNow()}
-               </span>
+      <div className=" videoMetaData__top">
+        <div className="py-1 d-flex justify-content-between align-items-center">
+          <span className=" days-ago">{moment(publishedAt).fromNow()}</span>
 
-               <div className='videoMetaData__top__stats'>
-                   <span className='mr-4' style={{color: '#fff'}}>
-                       <FaEye size={18} className='mr-2 eye-icon' /> 
-                   {numeral( viewCount).format('0.a')} views
-                   </span>
+          <div className="videoMetaData__top__stats">
+            <span className="mr-4" style={{ color: "#fff" }}>
+              <FaEye size={18} className="mr-2 eye-icon" />
+              {numeral(viewCount).format("0.a")} views
+            </span>
 
-                  <span className='mr-4 ' style={{color: '#FE3636'}}>
-                     <FaHeart size={18} className='mr-2 heart-icon' /> {numeral( likeCount ).format('0.a')}
-                  </span>
-                  <span style={{color: '#838080'}}>
-                     <FaHeartBroken size={18} className='mr-2 heartbr-icon' />{' '}
-                     {numeral(
-                         dislikeCount
-                       
-                         ).format('0.a')}
-                  </span>
-               </div>
-            </div>
-            
-            <h5>
-                {title}
-               
-                </h5>
-           
-         </div>
-         <div className='py-3 my-2 videoMetaData__channel d-flex justify-content-between align-items-center'>
-            <div className='d-flex'>
-               <img
-                  src= {channelSnippet?.thumbnails?.default?.url}
-                  alt=''
-                  className='mr-3' style={{borderRadius: '15px'}}
-               />
-               <div className='d-flex flex-column'>
-                  <span className='channel-title'>
-                      
-                      {channelTitle}
-                      </span>
-                  <span>
-                     {' '}
-                     {numeral(
-                         
-                         channelStatistics?.subscriberCount
-                         ).format(
-                        '0.a'
-                     )}{' '}
-                     Subscribers
-                  </span>
-               </div>
-            </div>
+            <span className="mr-4 " style={{ color: "#FE3636" }}>
+              <FaHeart size={18} className="mr-2 heart-icon" />{" "}
+              {numeral(likeCount).format("0.a")}
+            </span>
+            <span style={{ color: "#838080" }}>
+              <FaHeartBroken size={18} className="mr-2 heartbr-icon" />{" "}
+              {numeral(dislikeCount).format("0.a")}
+            </span>
+          </div>
+        </div>
 
-            <button
-               className={`p-2 m-2 border-0 btn
-                ${
-                  subscriptionStatus && 'btn-green'
-               }`
-               }
-               >
-               {subscriptionStatus ? 'Subscribed' : 'Subscribe'}
-               
-            </button>
-         </div>
-         <div className='videoMetaData__description'>
-            <ShowMoreText
-               lines={3}
-               more='SHOW MORE'
-               less='SHOW LESS'
-               anchorClass='showMoreText'
-               expanded={false}>
-               {description}
-           
-            </ShowMoreText>
-         </div>
+        <h5>{title}</h5>
       </div>
-   )
-}
+      <div className="py-3 my-2 videoMetaData__channel d-flex justify-content-between align-items-center">
+        <div className="d-flex">
+          <img
+            src={channelSnippet?.thumbnails?.default?.url}
+            alt=""
+            className="mr-3"
+            style={{ borderRadius: "15px" }}
+          />
+          <div className="d-flex flex-column">
+            <span className="channel-title">{channelTitle}</span>
+            <span>
+              {" "}
+              {numeral(channelStatistics?.subscriberCount).format("0.a")}{" "}
+              Subscribers
+            </span>
+          </div>
+        </div>
 
-export default VideoMetaData
+        <button
+          className={`p-2 m-2 border-0 btn
+                ${subscriptionStatus && "btn-green"}`}
+        >
+          {subscriptionStatus ? "Subscribed" : "Subscribe"}
+        </button>
+      </div>
+      <div className="videoMetaData__description">
+        <ShowMoreText
+          lines={3}
+          more="SHOW MORE"
+          less="SHOW LESS"
+          anchorClass="showMoreText"
+          expanded={false}
+        >
+          {description}
+        </ShowMoreText>
+      </div>
+    </div>
+  );
+};
+
+export default VideoMetaData;
--- src/screens/loginScreen/LoginScreen.js
+++ src/screens/loginScreen/LoginScreen.js
@@ -1,49 +1,53 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { login } from '../../redux/actions/auth.action';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { login } from "../../redux/actions/auth.action";
 
-import './loginScreen.scss';
+import "./loginScreen.scss";
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
 
-const dispatch = useDispatch()
+  const accessToken = useSelector((state) => state.auth.accessToken);
 
-const accessToken = useSelector(state => state.auth.accessToken)
+  const handleLogin = () => {
+    dispatch(login());
+  };
+  const history = useHistory();
 
-
-    const handleLogin = () =>{
-        dispatch(login())
+  useEffect(() => {
+    if (accessToken) {
+      history.push("/");
     }
-    const history = useHistory()
-   
-    useEffect(()=>{
-        if(accessToken){
-            history.push('/')
-        }
-    },[accessToken,history])
+  }, [accessToken, history]);
 
+  return (
+    <div className="login">
+      <img src="stems/utu.png" alt="" />
 
-    return (
-        <div className="login">
-            <img src="stems/utu.png" alt="" />
-            
-            <div className="login__container">
-                <img className="banner" src="stems/banner.svg" alt=""/>
-                <p className="creator">created by <strong>
-                    NSHCHY
-                    </strong>
-                    </p>
-                <button onClick={handleLogin}>Login with
-                <img src="stems/google.png" alt=""/>
-                </button>
-
-                <p>Based on Youtube Data Api</p>
-                
-            </div>
-            
+      <div className="login__container">
+        <img className="banner" src="stems/banner.svg" alt="" />
+        <p className="creator">
+          created by <strong>NSHCHY</strong>
+        </p>
+        <button onClick={handleLogin}>
+          Login with
+          <img src="stems/google.png" alt="" />
+        </button>
+        <div className="cred-box">
+          <div>
+            <img src="stems/instuct.png" alt="i icon" className="instruct" />
+            <span>Demo Credentials</span>
+          </div>
+          <div>
+            <span>Username : testmyproject.24@gmail.com</span>
+            <span>Password : test.test.test</span>
+          </div>
         </div>
-    )
-}
+        <p>Based on Youtube Data Api</p>
+      </div>
+    </div>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
